f51c084dda7260567b3ad342fba7c921f155f091
liuqiang <joe@3labs.wtf>
2025-02-07 16:37:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     print_hi('PyCharm111')
 
 
-    print_hi('PyCharm111?1111')
+    print_hi('PyCharm')
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
