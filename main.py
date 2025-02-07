348434c688538ad3a48888454973d216d18e9563
joe@3labs.wtf <joe@3labs.wtf>
2025-02-07 16:49:58 +0800

    test work flow

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharmaaa')
-    print_hi('PyCharm111')
+    print_hi('PyCharm111------')
 
 
     print_hi('PyCharm22')
